885942adb367fc44997451a4c2a6266ae182d465
mariecode1 <kairouzmariee@gmail.com>
2022-11-09 09:58:15 +0100

    page connexion

---
+++ CGU.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CGU HealthyVibe</title>
+    <link rel="stylesheet" href="CGU.css">
+</head>
+
+<body>
+    <h1>POLITIQUE DE CONFIDENTIALITÉ DU SITE OFFICIEL DE HealthyVibe</h1>
+
+    <h2>ARTICLE 1 : PRÉAMBULE</h2>
+    <p>
+        Cette politique de confidentialité s'applique au site : HealthyVibe. <br>
+        La présente politique de confidentialité a pour but d'exposer aux utilisateurs du site :
+        <ul>
+            <li>La manière dont sont collectées et traitées leurs données à caractère personnel. Doivent être considérées comme données personnelles toutes les données étant susceptibles d'identifier un utilisateur. Il s'agit notamment du prénom et du nom, de l'âge, de l'adresse postale, l'adresse mail, la localisation de l'utilisateur ou encore son adresse IP ;</li>
+            <li>Quels sont les droits des utilisateurs concernant ces données ;</li>
+            <li>Qui est responsable du traitement des données à caractère personnel collectées et traitées ;</li>
+            <li> A qui ces données sont transmises ;</li>
+            <li>Eventuellement, la politique du site en matière de fichiers "cookies".</li>
+        </ul>
+
+    </p>
+
+    <h2>ARTICLE 2 : PRINCIPES GÉNÉRAUX EN MATIÈRE DE COLLECTE ET DE TRAITEMENT DE DONNÉES </h2>
+    <p>
+        Conformément aux dispositions de l'article 5 du Règlement européen 2016/679, la collecte et le traitement des données des utilisateurs du site respectent les principes suivants : Licéité, loyauté et transparence : les données ne peuvent être collectées et traitées qu'avec le consentement de l'utilisateur propriétaire des données. A chaque fois que des données à caractère personnel seront collectées, il sera indiqué à l'utilisateur que ses données sont collectées, et pour quelles raisons ses données sont collectées ; 
+        <ul>
+            <li>Finalités limitées : la collecte et le traitement des données sont exécutés pour répondre à un ou plusieurs objectifs déterminés dans les présentes conditions générales d'utilisation; </li>
+            <li>Minimisation de la collecte et du traitement des données : seules les données nécessaires à la bonne exécution des objectifs poursuivis par le site sont collectées ; </li>
+            <li>Conservation des données réduites dans le temps : les données sont conservées pour une durée limitée, dont l'utilisateur est informé. Lorsque cette information ne peut pas être communiquée, l'utilisateur est informé des critères utilisés pour déterminer la durée de conservation ; </li>
+            <li>Intégrité et confidentialité des données collectées et traitées : le responsable du traitement des données s'engage à garantir l'intégrité et la confidentialité des données collectées. Afin d'être licites, et ce conformément aux exigences de l'article 6 du règlement européen 2016/679, la collecte et le traitement des données à caractère personnel ne pourront intervenir que s'ils respectent au moins l'une des conditions ci-après énumérées :</li>
+            <li>L'utilisateur a expressément consenti au traitement ;</li>
+            <li>Le traitement est nécessaire à la bonne exécution d'un contrat ;</li>
+            <li>Le traitement répond à une obligation légale ;</li>
+            <li>Le traitement s'explique par une nécessité liée à la sauvegarde des intérêts vitaux de la personne concernée ou d'une autre personne physique ;</li>
+            <li>Le traitement peut s'expliquer par une nécessité liée à l'exécution d'une mission d'intérêt public ou qui relève de l'exercice de l'autorité publique ;</li>
+            <li>Le traitement et la collecte des données à caractère personnel sont nécessaires aux fins des intérêts légitimes et privés poursuivis par le responsable du traitement ou par un tiers.</li>  
+        </ul>
+    </p>
+
+    <h2>ARTICLE 3 : DONNÉES À CARACTÈRE PERSONNEL COLLECTÉES ET TRAITÉES DANS LE CADRE DE LA NAVIGATION SUR LE SITE</h2>
+    <h3>A. DONNÉES COLLECTÉES ET TRAITÉES ET MODE DE COLLECTE </h3>
+    <p>
+        Les données à caractère personnel collectées sur le site <strong>Healthy Vibe</strong> sont les suivantes
+        <ul>
+            <li>Nom, Prénom</li>
+            <li>Date de naissance</li>
+            <li>Adresse Mail</li>
+            <li>Code Postal</li>
+        </ul>  
+        <br>
+        Ces données sont collectées lorsque l'utilisateur effectue l'une des opérations suivantes sur le site : 
+        <ul>
+            <li>Rythme cardiaque</li>
+            <li>Température corporelle</li>
+            <li>Exposition sonore</li>
+            <li>Données environnementales</li>
+        </ul> 
+        <br>
+        Le responsable du traitement conservera dans ses systèmes informatiques du site et dans des conditions raisonnables de sécurité l'ensemble des données collectées pour une durée de : 3 ans. 
+        <br>La collecte et le traitement des données répondent aux finalités suivantes : 
+        <ul>
+            <li>Etablissement de statistiques pour que le client puisse consulter son activité en moyenne</li>
+            <li>Etablissement de statistiques, tout en respectant l'anonymat du client Les traitements de données effectués sont fondés sur les bases légales du consentement de l'utilisateur</li>
+        </ul>
+
+    </p>
+
+    <h3>B. TRANSMISSION DES DONNÉES A DES TIERS</h3>
+    <p>Les données à caractère personnel collectées par le site ne sont transmises à aucun tiers, et ne sont traitées que par l'éditeur du site.</p>
+
+    <h3>C. HÉBERGEMENT DES DONNÉES</h3>
+    <p>
+        Le site Healthy Vibe est hébergé par : Infinite Measures, <br>
+        Dont le siège est situé à l'adresse ci-après : 10 Rue de Vanves, 92130<br>
+        L'hébergeur peut être contacté au numéro de téléphone suivant : +33 7 49 11 87 88.<br>
+        Les données collectées et traitées par le site sont exclusivement hébergées et traitées en France.<br>
+    </p>
+
+    <h2>ARTICLE 4 : RESPONSABLE DU TRAITEMENT DES DONNÉES</h2>
+    <h3>A. LE RESPONSABLE DU TRAITEMENT DES DONNÉES</h3>
+    <p>Le responsable du traitement des données à caractère personnel est : Maxence GUEGUEN.
+        Il peut être contacté par mail à l'adresse suivante : <email>maxence.gueguen@eleve.isep.fr</email>
+        Le responsable du traitement des données est chargé de déterminer les finalités et les moyens mis au service du traitement des données à caractère personnel.
+    </p>
+
+    <h3>B. OBLIGATIONS DU RESPONSABLE DU TRAITEMENT DES DONNÉES</h3>
+    <p>Le responsable du traitement s'engage à protéger les données à caractère personnel collectées, à ne pas les transmettre à des tiers sans que l'utilisateur n'en ait été informé et à respecter les finalités pour lesquelles ces données ont été collectées.
+        De plus, le responsable du traitement des données s'engage à notifier l'utilisateur en cas de rectification ou de suppression des données, à moins que cela n'entraîne pour lui des formalités, coûts et démarches disproportionnés.<br>
+        Dans le cas où l'intégrité, la confidentialité ou la sécurité des données à caractère personnel de l'utilisateur est compromise, le responsable du traitement s'engage à informer l'utilisateur par tout moyen.
+    </p>
+
+    <h2>ARTICLE 5 : DROITS DE L'UTILISATEUR </h2>
+    <p>Conformément à la réglementation concernant le traitement des données à caractère personnel, l'utilisateur possède les droits ci-après énumérés.<br>
+        Afin que le responsable du traitement des données fasse droit à sa demande, l'utilisateur est tenu de lui communiquer : ses prénom et nom ainsi que son adresse e-mail, et si cela est pertinent, son numéro de compte ou d'espace personnel ou d'abonné.<br>
+        Le responsable du traitement des données est tenu de répondre à l'utilisateur dans un délai de 30 (trente) jours maximum.
+   </p> 
+
+   <h3>A. PRÉSENTATION DES DROITS DE L'UTILISATEUR EN MATIÈRE DE COLLECTE ET TRAITEMENT DE DONNÉES</h3>
+    <h4>a. Droit d'accès, de rectification et droit à l'effacement</h4>
+    <p>
+        L'utilisateur peut prendre connaissance, mettre à jour, modifier ou demander la suppression des données le concernant, en respectant la procédure ci-après énoncée :
+
+        <br><br>S'il en possède un, l'utilisateur a le droit de demander la suppression de son espace personnel en suivant, contactez nous via le site.    
+    </p>
+
+    <h4>b. Droit à la portabilité des données</h4>
+    <p>L'utilisateur a le droit de demander la portabilité de ses données personnelles, détenues par le site, vers un autre site.</p>
+    <h4>c. Droit à la limitation et à l'opposition du traitement des données</h4>
+    <p>L'utilisateur a le droit de demander la limitation ou de s'opposer au traitement de ses données par le site, sans que le site ne puisse refuser, sauf à démontrer l'existence de motifs légitimes et impérieux, pouvant prévaloir sur les intérêts et les droits et libertés de l'utilisateur.</p>
+    <h4>d. Droit de ne pas faire l'objet d'une décision fondée exclusivement sur un procédé automatisé</h4>
+    <p>Conformément aux dispositions du règlement 2016/679, l'utilisateur a le droit de ne pas faire l'objet d'une décision fondée exclusivement sur un procédé automatisé si la décision produit des effets juridiques le concernant, ou l'affecte de manière significative de façon similaire.</p>
+    <h4>e. Droit de saisir l'autorité de contrôle compétente</h4>
+    <p>Dans le cas où le responsable du traitement des données décide de ne pas répondre à la demande de l'utilisateur, et que l'utilisateur souhaite contester cette décision, ou, s'il pense qu'il est porté atteinte à l'un des droits énumérés ci-dessus, il est en droit de saisir la CNIL (Commission Nationale de l'Informatique et des Libertés, https://www.cnil.fr) ou tout juge compétent.</p>
+    <h3>B. DONNÉES PERSONNELLES DES PERSONNES MINEURES</h3>
+    <p>Conformément aux dispositions de l'article 8 du règlement européen 2016/679 et à la loi Informatique et Libertés, seuls les mineurs âgés de 15 ans ou plus peuvent consentir au traitement de leurs données personnelles.
+        Si l'utilisateur est un mineur de moins de 15 ans, l'accord d'un représentant légal sera requis afin que des données à caractère personnel puissent être collectées et traitées.
+        L'éditeur du site se réserve le droit de vérifier par tout moyen que l'utilisateur est âgé de plus de 15 ans, ou qu'il aura obtenu l'accord d'un représentant légal avant de naviguer sur le site.
+    </p>
+
+    <h2>ARTICLE 6 : CONDITIONS DE MODIFICATION DE LA POLITIQUE DE CONFIDENTIALITÉ</h2>
+    <p>
+        La présente politique de confidentialité peut être consultée à tout moment sur le site officiel de HealthyVibe. <br>
+        L'éditeur du site se réserve le droit de la modifier afin de garantir sa conformité avec le droit en vigueur. Par conséquent, l'utilisateur est invité à venir consulter régulièrement cette politique de confidentialité afin de se tenir informé des derniers changements qui lui seront apportés.<br>
+        Il est porté à la connaissance de l'utilisateur que la dernière mise à jour de la présente politique de confidentialité est intervenue le : <date>25/10/2022</date>.
+    </p>
+    
+</body>
