059e018082566e4862b855f5de809bc129f6c415
Syraps <115782856+Syraps@users.noreply.github.com>
2022-11-29 14:58:26 +0100

    html et css de l'espace santée + img temporaire

---
+++ espaceSantee.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <meta charset="UTF-8">
+    <title>HealthyVibe</title>
+    <link rel="stylesheet" href="res/css/espaceSantee.css">
+    <script src="res/js/script.js"></script>
+</head>
+
+<body>  
+<?php
+    include './res/php/header.php';
+    ?>
+    <div id="spaceHolder">
+    <div id="calendrier">
+        <div id="liens">
+         <a href="./accueil.php">Accueil</a>
+         <a href="./vosDonnees.php">Espace Santé</a> 
+        </div>
+            <table>
+            <tr>
+            <th>L</th><th>M</th><th>M</th><th>J</th><th>V</th><th>S</th><th>D</th>
+            </tr>
+            <tr>
+            <td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td>
+            </tr>
+            <tr>
+            <td>8</td><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td>
+            <tr>
+            <td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td>
+            <tr>
+            <td>22</td><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td>
+            <tr>
+            <td>29</td><td>30</td><td>31</td><td></td><td></td><td></td><td></td>
+            </table>
+            <div class="bouttontemps">
+                <input type="button" class="enSavoirPlus" name="enSavoirPlus" value="Semaine" class="enSavoirPlus">
+                <input type="button" class="enSavoirPlus" name="enSavoirPlus" value="Mois" class="enSavoirPlus">
+            </div>
+        </div>
+        <div id=carreblanc>
+        <div id="graph1">
+            <img src="./res/img/graph.png" alt="graph 1">
+           <div id="pres">
+            <img class=icone src="./res/img/temperature.png" alt="idéogramme thermomètre">
+            <p><strong>37°C</strong></p>
+            </div>
+            <input type="button" class="btn" value="Tableau">
+        </div>
+        <div id="graph2">
+            <img src="./res/img/graph.png" alt="graph 2">
+            <div id="pres">
+            <img class=icone src="./res/img/headphones.png" alt="idéogramme casque">
+            <p><strong>79Db</strong></p>
+            </div>
+            <input type="button" class="btn" value="Tableau">
+        </div>
+        <div id="graph3">
+            <img src="./res/img/graph.png" alt="graph 3">
+            <div id="pres">
+            <img class=icone src="./res/img/heart.png" alt="idéogramme coeur">
+            <p><strong>85 BPM</strong></p>
+            </div>
+            <input type="button" class="btn" value="Tableau">
+        </div>
+        </div>
+    
+    </div>
+    <?php
+    include './res/php/footer.php';
+    ?>
+</body>
+</html>
