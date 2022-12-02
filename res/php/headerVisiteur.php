cb50f86716092496ee28297682346c05fd71de90
Jifoux <jfgold1101@gmail.com>
2022-12-02 08:45:56 +0100

    Nouvel Header pour pages visiteur (hors pages inscription / connexion)

---
+++ res/php/headerVisiteur.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Header Visiteur</title>
+    <link rel="stylesheet" href="/HealthyVibe/res/css/headerVisiteur.css">
+</head>
+
+<body>
+    <header>
+        <div id="ligneVerte"></div>
+        <img src="/HealthyVibe/res/img/logo.png" alt="Logo de HealthyVibe">
+        <nav>
+            <a href="">Tips ecologiques</a>
+            <a href="">FAQ</a>
+            <a href="/HealthyVibe/connexion.php" id="co">Se connecter</a>
+            <a href="/HealthyVibe/inscription.php" id="inscrire">S'inscrire</a>
+        </nav>
+    </header>
+    
+</body>
+</html>
