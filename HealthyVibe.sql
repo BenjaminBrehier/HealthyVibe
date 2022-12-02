833cbd36816bf63c08ae84341f9fa6facf626ae7
BenjaminBrehier <benbrehier@gmail.com>
2022-12-02 09:18:04 +0100

    Ajout Creation Database

--- HealthyVibe.sql
+++ HealthyVibe.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS UTILISATEUR (
+CREATE DATABASE IF NOT EXISTS HealthyVibe;
+
+CREATE TABLE IF NOT EXISTS HealthyVibe.UTILISATEUR (
   idUtilisateur INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nom VARCHAR(42),
   prenom VARCHAR(42),
@@ -12,95 +14,95 @@ CREATE TABLE IF NOT EXISTS UTILISATEUR (
   banni BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS SUJET (
+CREATE TABLE IF NOT EXISTS HealthyVibe.SUJET (
   idSujet INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   titre VARCHAR(42),
   datecreation DATETIME,
   datemodification DATETIME,
   status BOOLEAN,
   idUtilisateur INT,
-  FOREIGN KEY (idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur)
+  FOREIGN KEY (idUtilisateur) REFERENCES HealthyVibe.UTILISATEUR(idUtilisateur)
 );
 
-CREATE TABLE IF NOT EXISTS CASQUE (
+CREATE TABLE IF NOT EXISTS HealthyVibe.CASQUE (
   idCasque INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   dateachat DATETIME,
   idUtilisateur INT,
-  FOREIGN KEY (idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur)
+  FOREIGN KEY (idUtilisateur) REFERENCES HealthyVibe.UTILISATEUR(idUtilisateur)
 );
 
-CREATE TABLE IF NOT EXISTS CAPTEUR (
+CREATE TABLE IF NOT EXISTS HealthyVibe.CAPTEUR (
   idCapteur INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nom VARCHAR(42),
   type VARCHAR(42),
   idCasque INT,
-  FOREIGN KEY (idCasque) REFERENCES CASQUE(idCasque)
+  FOREIGN KEY (idCasque) REFERENCES HealthyVibe.CASQUE(idCasque)
 );
 
-CREATE TABLE IF NOT EXISTS DONNEE (
+CREATE TABLE IF NOT EXISTS HealthyVibe.DONNEE (
   idDonnee INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   valeur FLOAT,
   date DATETIME,
   idCapteur INT,
-  FOREIGN KEY (idCapteur) REFERENCES CAPTEUR(idCapteur)
+  FOREIGN KEY (idCapteur) REFERENCES HealthyVibe.CAPTEUR(idCapteur)
 );
 
-CREATE TABLE IF NOT EXISTS FAQ (
+CREATE TABLE IF NOT EXISTS HealthyVibe.FAQ (
   idFaq INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   question TEXT,
   réponse TEXT
 );
 
-CREATE TABLE IF NOT EXISTS QUIZ (
+CREATE TABLE IF NOT EXISTS HealthyVibe.QUIZ (
   idQuiz INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nom VARCHAR(42)
 );
 
-CREATE TABLE IF NOT EXISTS COMPLETE (
+CREATE TABLE IF NOT EXISTS HealthyVibe.COMPLETE (
   idQuiz INT,
   idUtilisateur INT,
-  FOREIGN KEY (idQuiz) REFERENCES QUIZ(idQuiz),
-  FOREIGN KEY (idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur)
+  FOREIGN KEY (idQuiz) REFERENCES HealthyVibe.QUIZ(idQuiz),
+  FOREIGN KEY (idUtilisateur) REFERENCES HealthyVibe.UTILISATEUR(idUtilisateur)
 );
 
-CREATE TABLE IF NOT EXISTS QUESTION (
+CREATE TABLE IF NOT EXISTS HealthyVibe.QUESTION (
   idQuestion INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   question TEXT,
   reponse TEXT
 );
 
-CREATE TABLE IF NOT EXISTS COMPOSE (
+CREATE TABLE IF NOT EXISTS HealthyVibe.COMPOSE (
   idQuiz INT,
   idQuestion INT,
-  FOREIGN KEY (idQuiz) REFERENCES QUIZ(idQuiz),
-  FOREIGN KEY (idQuestion) REFERENCES QUESTION(idQuestion),
+  FOREIGN KEY (idQuiz) REFERENCES HealthyVibe.QUIZ(idQuiz),
+  FOREIGN KEY (idQuestion) REFERENCES HealthyVibe.QUESTION(idQuestion),
   PRIMARY KEY (idQuiz,idQuestion)
 );
 
-CREATE TABLE IF NOT EXISTS MESSAGEDIRECT (
+CREATE TABLE IF NOT EXISTS HealthyVibe.MESSAGEDIRECT (
   idUtilisateur INT,
   idUtilisateur1 INT,
   date DATETIME,
   contenu TEXT,
-  FOREIGN KEY (idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur),
-  FOREIGN KEY (idUtilisateur1) REFERENCES UTILISATEUR(idUtilisateur),
+  FOREIGN KEY (idUtilisateur) REFERENCES HealthyVibe.UTILISATEUR(idUtilisateur),
+  FOREIGN KEY (idUtilisateur1) REFERENCES HealthyVibe.UTILISATEUR(idUtilisateur),
   PRIMARY KEY (idUtilisateur,idUtilisateur1)
 );
 
-CREATE TABLE IF NOT EXISTS POST (
+CREATE TABLE IF NOT EXISTS HealthyVibe.POST (
   idPost INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   date DATETIME,
   contenu TEXT,
   idUtilisateur INT,
   idSujet INT,
   idReponse INT,
-  FOREIGN KEY (idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur),
-  FOREIGN KEY (idSujet) REFERENCES SUJET(idSujet),
-  FOREIGN KEY (idReponse) REFERENCES POST(idPost)
+  FOREIGN KEY (idUtilisateur) REFERENCES HealthyVibe.UTILISATEUR(idUtilisateur),
+  FOREIGN KEY (idSujet) REFERENCES HealthyVibe.SUJET(idSujet),
+  FOREIGN KEY (idReponse) REFERENCES HealthyVibe.POST(idPost)
 );
 
-CREATE USER 'adminHealthyVibe'@'localhost' IDENTIFIED BY 'adminHealthyVibe';
+CREATE USER IF NOT EXISTS 'adminHealthyVibe'@'localhost' IDENTIFIED BY 'adminHealthyVibe';
 GRANT ALL PRIVILEGES ON HeathyVibe . * TO 'adminHealthyVibe'@'localhost';
 
-INSERT INTO `UTILISATEUR` (`idUtilisateur`, `nom`, `prenom`, `email`, `mdp`, `tel`, `adresse`, `codepostal`, `datenaissance`, `role`, `banni`) VALUES
-(14, 'AdminNom', 'Admin', 'admin@gmail.com', 'admin', '192168', '1 rue du Web', 192, '1970-01-01', 0, 0);
+INSERT INTO HealthyVibe.UTILISATEUR (`idUtilisateur`, `nom`, `prenom`, `email`, `mdp`, `tel`, `adresse`, `codepostal`, `datenaissance`, `role`, `banni`) VALUES
+(1, 'AdminNom', 'Admin', 'admin@gmail.com', 'admin', '192168', '1 rue du Web', 192, '1970-01-01', 0, 0);
