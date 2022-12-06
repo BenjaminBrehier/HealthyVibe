c4aac523d724d77c0138f16b12cff1f1ed58e889
mariecode1 <kairouzmariee@gmail.com>
2022-12-06 09:48:35 +0100

    bug

---
+++ FAQ.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>HealthyVibe</title>
+    <link rel="stylesheet" href="res/css/FAQ.css">
+    <script src="res/js/script.js"></script>
+</head>
+
+<body>
+<?php
+    include './res/php/header.php';
+    ?>
+    <div id='espaceRecherche'>
+        <input type='text' value='Rechercher une question' id='barreRecherche'>
+        <div id='listeQuestions'>
+            <ul>
+                <li> Question 1</li>
+                <li> Question 1</li>
+                <li> Question 1</li>
+                <li> Question 1</li>
+            </ul>
+        </div>
+
+        <div id='decoImage'>
+            <img src='./casque.jpeg'>
+        </div>
+
+    </div>
+
+
+
+<?php
+        include './res/php/footer.php';
+        ?>
+</body>
+</html>
