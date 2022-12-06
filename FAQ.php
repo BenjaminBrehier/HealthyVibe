4f71e9b1c2a4b8297c4bf6aed020eb8cd35d1af7
mariecode1 <kairouzmariee@gmail.com>
2022-12-06 09:28:30 +0100

    Fichier FAQ (php et css)

---
+++ FAQ.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>HealthyVibe</title>
+    <link rel="stylesheet" href="res/css/FAQ.css">
+    <script src="res/js/script.js"></script>
+</head>
+
+<body>
+<?php
+    include './res/php/header.php';
+    ?>
+
+
+
+<?php
+        include './res/php/footer.php';
+        ?>
+</body>
+</html>
