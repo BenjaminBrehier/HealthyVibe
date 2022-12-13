015c61b1349794407783fbc75ff105041d45e7c5
gadjialoumatou02 <gadjialoumatou02@gmail.com>
2022-12-13 21:54:59 +0100

    Modification de la police et la taille du texte

--- infinitemeasures.php
+++ infinitemeasures.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -6,25 +9,35 @@
     <title>Infinite measures</title>
     <link rel="stylesheet" href="res/css/infinitemeasures.css">
 </head>
-
 <body>
-     <h1>Infinite measures</h1>
+    <?php
+    include './res/php/header.php';
+    ?>
+    <div class="infinite">
+        <h1>A propos de INFINITE MEASURES </h1>
+    </div>
+    <section class="infinites4">
+        <div class="up">
+            <p><img src="./res/img/infinite_measures.png" alt="infinites_measures" class="image">Infinite measures est l'une des sociétés qui travaillent dans le monde numérique et informatique tout en prenant en compte les questions ergonomiques et environnementales</p>
+        </div>
+    </section>
+
+
     <section class="infinites1">
-        <div>
-            <p><img src="./res/img/infinite_measures.png" alt="logo infinite_measures" class="image">Nous sommes une sociéte installateur de solutions qui conçoit ses propres systèmes</p>
+        <div class="up">
+            <p><img src="./res/img/infinites_sys.jpg" alt="infinites_sys" class="image">Nous sommes une sociéte installateur de solutions qui conçoit ses propres systèmes</p>
         </div>
     </section>
     <section class="infinites2">
-        <div>
-            <p><img src="./res/img/infinites_elec.jpg" alt="" class="image">Infinite measures a une parfaite maîtrise des besoins des utilisateurs qui lui permet de mettre en place les systèmes adéquats pour répondre à ces besoins</p>
+        <div class="up" >
+            <p><img src="./res/img/infinites_elec.jpg" alt="infinites_elec" class="image">Infinite measures a une parfaite maîtrise des besoins des utilisateurs qui lui permet de mettre en place les systèmes adéquats pour répondre à ces besoins</p>
         </div>
     </section>
     <section class="infinites3">
-        <div>
-            <p><img src="./res/img/infinites_telecom.jpg" alt="logo infinite_measures" class="image">Nous sommes une sociéte avec de grandes compétences en électronique, informatique, traitement de signal et en télécommunications assurant la bonne mise en place de  nos systèmes</p>
+        <div class="up">
+            <p><img src="./res/img/infinites_telecom.jpg" alt="infinite_telecom" class="image">Nous sommes une sociéte avec de grandes compétences en électronique, informatique, traitement de signal et en télécommunications assurant la bonne mise en place de  nos systèmes</p>
         </div>
     </section>
-   
 </body>
 
 </html>
