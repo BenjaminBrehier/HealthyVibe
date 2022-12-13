acf884b27db992e9a8b58d392bfbbcd9d9a7ef82
gadjialoumatou02 <gadjialoumatou02@gmail.com>
2022-12-13 13:02:58 +0100

    page infinitemeasures modification footer,profil,tipsecolo

--- res/js/script.js
+++ res/js/script.js
@@ -1,25 +1,26 @@
-// L'image img#image
-var image = document.getElementById("image");
+ // L'image img#image
+ var image = document.getElementById("image");
+     
+ // La fonction previewPicture
+ var previewPicture  = function (e) {
 
-// La fonction previewPicture
-var previewPicture = function (e) {
-    // e.files contient un objet FileList
-    const [picture] = e.files
+     // e.files contient un objet FileList
+     const [picture] = e.files
 
-    // "picture" est un objet File
-    if (picture) {
-        // On change l'URL de l'image
-        image.src = URL.createObjectURL(picture)
+     // "picture" est un objet File
+     if (picture) {
+
+         // L'objet FileReader
+         var reader = new FileReader();
+
+         // L'événement déclenché lorsque la lecture est complète
+         reader.onload = function (e) {
+             // On change l'URL de l'image (base64)
+             image.src = e.target.result
+         }
+
+         // On lit le fichier "picture" uploadé
+         reader.readAsDataURL(picture)
+
+     }
     }
-}
-
-
-function afficher(x) {
-    window.location.href = "./afficheSujet.php?idSujet=" + x;
-}
-
-function deletePost(x, idSujet) {
-    if (confirm("Voulez vous vraiment supprimer ?") == true) {
-        window.location.href = "./res/php/deletePost.php?idPost=" + x+"&idSujet="+idSujet;
-    }
-}
