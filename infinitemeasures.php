432eb946cab724f9da05536e946aca2cb621b46c
aymaneh22 <aymaneh.ah@gmail.com>
2022-12-15 20:19:22 +0100

    Modification de la page "a propos de"

--- infinitemeasures.php
+++ infinitemeasures.php
@@ -13,31 +13,43 @@ session_start();
     <?php
     include './res/php/header.php';
     ?>
-    <div class="infinite">
-        <h1>A propos de INFINITE MEASURES </h1>
+    <h1> A propos de INFINITE MEASURE</h1>
+    <section id="fonctions">
+    <div class="infinites1">
+            <div>
+                <p>Infinite measures est l'une des sociétés qui travaillent dans le monde numérique et informatique tout en prenant en compte les questions ergonomiques et environnementales.</p>
+            </div>
+            <img src="./res/img/infinite_measures.png" alt="infinites_measures" class="image">
+            
+    </div> 
+    <div class="infinites2">
+            <img src="./res/img/infinites_sys.jpg" alt="infinites_sys" class="image">
+             <div>
+                <p>Nous sommes une sociéte installateur de solutions qui conçoit ses propres systèmes.</p>
+            </div>
+    </div> 
+
+    <div class="infinites1"> 
+         <div>
+             <p>Infinite measures a une parfaite maîtrise des besoins des utilisateurs qui lui permet de mettre en place les systèmes adéquats pour répondre à ces besoins </p>
+         </div>
+            <img src="./res/img/infinites_elec.jpg" alt="infinites_elec" class="image">>
     </div>
-    <section class="infinites4">
-        <div class="up">
-            <p><img src="./res/img/infinite_measures.png" alt="infinites_measures" class="image">Infinite measures est l'une des sociétés qui travaillent dans le monde numérique et informatique tout en prenant en compte les questions ergonomiques et environnementales</p>
-        </div>
-    </section>
+
+    <div class="infinites2">
+       <img src="./res/img/infinites_telecom.jpg" alt="infinite_telecom" class="image">   
+       <div>
+         <p>Nous sommes une sociéte avec de grandes compétences en électronique, informatique, traitement de signal et en télécommunications assurant la bonne mise en place de  nos systèmes </p>
+       </div>
+   </div>
 
 
-    <section class="infinites1">
-        <div class="up">
-            <p><img src="./res/img/infinites_sys.jpg" alt="infinites_sys" class="image">Nous sommes une sociéte installateur de solutions qui conçoit ses propres systèmes</p>
-        </div>
-    </section>
-    <section class="infinites2">
-        <div class="up" >
-            <p><img src="./res/img/infinites_elec.jpg" alt="infinites_elec" class="image">Infinite measures a une parfaite maîtrise des besoins des utilisateurs qui lui permet de mettre en place les systèmes adéquats pour répondre à ces besoins</p>
-        </div>
-    </section>
-    <section class="infinites3">
-        <div class="up">
-            <p><img src="./res/img/infinites_telecom.jpg" alt="infinite_telecom" class="image">Nous sommes une sociéte avec de grandes compétences en électronique, informatique, traitement de signal et en télécommunications assurant la bonne mise en place de  nos systèmes</p>
-        </div>
-    </section>
+    </section> 
+    <?php
+    include './res/php/footer.php';
+    ?>
+
+    
 </body>
 
 </html>
