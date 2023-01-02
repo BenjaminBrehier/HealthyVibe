850b82546e20b3a657600b3dddaa009a3381f442
BenjaminBrehier <benbrehier@gmail.com>
2023-01-02 15:21:50 +0100

    Coloration lien header page actuelle

--- res/php/header.php
+++ res/php/header.php
@@ -28,12 +28,15 @@
 
     <nav id='main'>
     <ul id="menu_horizontal">
-        <li><a href="./index.php" id="données">Accueil</a></li>
-        <li><a href="./vosDonnees.php" id="données">Vos données</a></li>
-        <li><a href="./forum.php" id="données">Forum</a></li>
-        <li><a href="./FAQ.php">FAQ</a></li>
-        <li><a href="./tipsEcologiques.php">Tips ecologiques</a></li>
-        <li><a href="./nousContacter.php">Nous contacter</a></li>
+        <?php 
+            $page = basename($_SERVER['PHP_SELF']);
+        ?>
+        <li><a href="./index.php" id="données" <?php if($page == "index.php") {echo 'class="selected"';}?>>Accueil</a></li>
+        <li><a href="./vosDonnees.php" id="données" <?php if($page == "vosDonnees.php") {echo 'class="selected"';}?>>Vos données</a></li>
+        <li><a href="./forum.php" id="données" <?php if($page == "forum.php") {echo 'class="selected"';}?>>Forum</a></li>
+        <li><a href="./FAQ.php" <?php if($page == "FAQ.php") {echo 'class="selected"';}?>>FAQ</a></li>
+        <li><a href="./tipsEcologiques.php" <?php if($page == "tipsEcologiques.php") {echo 'class="selected"';}?>>Tips ecologiques</a></li>
+        <li><a href="./nousContacter.php" <?php if($page == "nousContacter.php") {echo 'class="selected"';}?>>Nous contacter</a></li>
     </ul>
     </nav>
 
