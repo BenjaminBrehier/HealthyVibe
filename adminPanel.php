947b4ed2c9125807cfc86e3a3a323ee15b03dc67
mariecode1 <kairouzmariee@gmail.com>
2023-01-06 11:58:34 +0100

    adminpanel

--- adminPanel.php
+++ adminPanel.php
@@ -27,6 +27,9 @@ if (!isset($_SESSION['id']) || $_SESSION['role'] != 1) {
             <p>></p>
             <a href="">Panel Admin</a>
         </div>
+        <?php 
+        if (isset($_GET['onglet'])&& $_GET['onglet']=='Utilisateurs') {
+            ?>
         <h1>Liste des utilisateurs</h1>
         <table>
             <tr>
@@ -60,9 +63,99 @@ if (!isset($_SESSION['id']) || $_SESSION['role'] != 1) {
                 </tr>
             <?php
             }
-            ?>
+            ?> 
+        </table>             
+            <?php
+                }
+                ?>
 
-        </table>
+        <?php 
+        if (isset($_GET['onglet'])&& $_GET['onglet']=='Lieux') {
+            ?>
+        <h1>Lieux de vente des casques HealthyVibe</h1>
+        <table>
+            <tr>
+                <th>Adresse</th>
+                <th>Supprimer</th>
+            </tr>
+            <?php
+            $co = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+            $result = $co->query("SELECT * FROM lieuvente");
+            while ($row = $result->fetch_object()) {
+            ?> <tr>
+                    <td><?php echo $row->lieu; ?></td>
+                    <td><a href="<?php echo $row->idLieu; ?>">X</a></td>
+                </tr>
+            <?php
+            }
+            ?> 
+        </table>             
+            <?php
+                }
+                ?>
+                <?php 
+
+        if (isset($_GET['onglet'])&& $_GET['onglet']=='Casques') {
+            ?>
+        <h1>Casques et utilisateur correspondant</h1>
+        <table>
+            <tr>
+                <th>N° de casque</th>
+                <th>Nom utilisateur</th>
+                <th>Prénom utilisateur</th>
+                <th>Date d'achat</th>
+                <th>Etat</th>
+            </tr>
+            <?php
+            $co = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+            $result = $co->query("SELECT nom,prenom,idCasque,dateachat FROM utilisateur INNER JOIN casque ON utilisateur.idUtilisateur=casque.idUtilisateur");
+            while ($row = $result->fetch_object()) {
+            ?> <tr>
+                    <td><?php echo $row->idCasque; ?></td>
+                    <td><?php echo $row->nom; ?></td>
+                    <td><?php echo $row->prenom; ?></td>
+                    <td><?php echo $row->dateachat; ?></td>
+                    <td><?php echo $row->dateachat; ?></td>
+                </tr>
+            <?php
+            }
+            ?> 
+        </table>             
+            <?php
+                }
+                ?>
+
+<?php 
+        if (isset($_GET['onglet'])&& $_GET['onglet']=='Forum') {
+            ?>
+        <h1>Liste des sujets</h1>
+        <table>
+            <tr>
+                <th>idSujet</th>
+                <th>Titre</th>
+                <th>Date de création</th>
+                <th>Date de modification</th>
+                <th>Statut</th>
+
+            </tr>
+            <?php
+            $co = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+            $result = $co->query("SELECT * FROM sujet");
+            while ($row = $result->fetch_object()) {
+            ?> <tr>
+                    <td><?php echo $row->idSujet; ?></td>
+                    <td><?php echo $row->titre; ?></td>
+                    <td><?php echo $row->datecreation; ?></td>
+                    <td><?php echo $row->datemodification; ?></td>
+                    <td><?php echo $row->status; ?></td>
+            <?php
+            }
+            ?> 
+        </table>             
+            <?php
+                }
+                ?>
+
     </section>
 
     <?php
