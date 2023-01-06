36cafbda231ce032cb7d07317a0c753d03f86703
mariecode1 <kairouzmariee@gmail.com>
2023-01-06 09:50:10 +0100

    espace envi = header size

--- espaceEnvironnement.php
+++ espaceEnvironnement.php
@@ -31,74 +31,18 @@ if (!isset($_SESSION['id'])) {
         </div>
 
         <div id="spaceHolder">
-            <div id="colonneGauche">
-
-                <div id="calendrier">
-                    <table>
-                        <tr>
-                            <th>L</th>
-                            <th>M</th>
-                            <th>M</th>
-                            <th>J</th>
-                            <th>V</th>
-                            <th>S</th>
-                            <th>D</th>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>7</td>
-                        </tr>
-                        <tr>
-                            <td>8</td>
-                            <td>9</td>
-                            <td>10</td>
-                            <td>11</td>
-                            <td>12</td>
-                            <td>13</td>
-                            <td>14</td>
-                        </tr>
-                        <tr>
-                            <td>15</td>
-                            <td>16</td>
-                            <td>17</td>
-                            <td>18</td>
-                            <td>19</td>
-                            <td>20</td>
-                            <td>21</td>
-                        </tr>
-                        <tr>
-                            <td>22</td>
-                            <td>23</td>
-                            <td>24</td>
-                            <td>25</td>
-                            <td>26</td>
-                            <td>27</td>
-                            <td>28</td>
-                        </tr>
-                        <tr>
-                            <td>29</td>
-                            <td>30</td>
-                            <td>31</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </table>
-
-                    <div class="bouttontemps">
-                        <input type="button" class="enSavoirPlus" name="enSavoirPlus" value="Semaine" class="enSavoirPlus">
-                        <input type="button" class="enSavoirPlus" name="enSavoirPlus" value="Mois" class="enSavoirPlus">
-                    </div>
+            <div id='partie1'>
+                <div class="bouttontemps">
+                    <label for="période">
+                        <p class='text'>Choisissez une période</p>
+                    </label>
+                    <input type="date" class="date" placeholder='Date de début'>
+                    <p class='text'>à</p>
+                    <input type="date" class="date" placeholder='Date de fin'>
+                    <input type="button" class="enSavoirPlus" name="enSavoirPlus" value="Afficher les statistiques">
                 </div>
             </div>
-
-            <div id="colonneDroite">
+            <div id="partie2">
                 <div id="graph">
                     <img src="./res/img/graph.png" alt="graph1" class='graph1'>
                     <div id="pres">
