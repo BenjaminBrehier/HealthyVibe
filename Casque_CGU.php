2cdc6a60cee7468d4c2f89460bec5d19f33d9ab6
gadjialoumatou02 <gadjialoumatou02@gmail.com>
2023-01-25 18:56:54 +0100

    css du Casque_CGU

--- Casque_CGU.php
+++ Casque_CGU.php
@@ -25,11 +25,11 @@ session_start();
             <h3>Connexion et appairage</h3>
             <p>HealthyVibe est un casque non-filaire de dernières  générations aux fonctionalités très importantes pour un meilleur cadre de vie.La première chose à faire après avoir déballé votre casque Bluetooth est d'appuyer sur son bouton d'appairage. Une fois que celui-ci clignote, attrappez votre smartphone et activez le Bluetooth dans les paramètres. Votre casque doit apparaître dans la liste, il suffit de cliquer dessus pour lancer la connexion. Non seulement l'apparairage c'est simple, mais en plus, une fois que c'est fait, plus besoin de naviguer dans les menus de votre smartphone. Il suffit simplement d'activer le bluetooth et d'allumer votre casque.
         </div>
-        <img src="./res/img/casque.jpg" alt="casque" class="image">
+        <img src="./res/img/casque.jpg" alt="casque" >
             
     </div> 
     <div class="infinites2">
-        <img src="./res/img/casque&charge.jpg" alt="casque" class="image">
+        <img src="./res/img/casque&charge.jpg" alt="casque">
         <div class='info2'>
             <h3>Recharge</h3>
             <p>HealthyVibe est casque qui vous permet d'échanger avec votre smartphone  afin de communiquer des données qui vont seront visualisées sur un site web une fois connecté au site. Vous pouvez le recharger avec son cable USB C, ce qui sera très rare à cause de sa longue durée d'autonomie</p>
@@ -41,7 +41,7 @@ session_start();
             <h3>Données et Usage</h3>
             <p> L'acquisition des données se fait grâce à des capteurs qui sont installés sur le casque. Ainsi ils prennent des mesures sur la qualité de votre milieu (la température, l'humidité, la fréquence cardiaque, le débit sonore et la teneur en monoxyde de carbone) permettant à l'individu d'apprécier son milieu. En plus, vous pouvez user de votre casque pour écouter tous vos fichiers audios et passer des appels avec. </p>
         </div>
-        <img src="./res/img/fonctionnalite_6.jpg" alt="fonctionalite6" class="image">
+        <img src="./res/img/fonctionnalite_6.jpg" alt="fonctionalite6" >
     </div>
     </section> 
 
